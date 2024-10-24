689a806c73c6167d71eed3e10a2b71e8c52a2cad
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-10-24 03:15:35 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
+""" 12. Log stats
+    This script connects to a MongoDB database to retrieve and display
+    statistics about Nginx logs, including the total number of logs and
+    counts for different HTTP methods.
+"""
 
-def main():
-    # Connect to MongoDB
-    client = MongoClient('localhost', 27017)
-    db = client.logs  # Database name
-    collection = db.nginx  # Collection name
+from pymongo import MongoClient, errors
 
-    # Get total number of logs
-    total_logs = collection.count_documents({})
 
-    # Get count for each method
-    methods_count = {
-        "GET": collection.count_documents({"method": "GET"}),
-        "POST": collection.count_documents({"method": "POST"}),
-        "PUT": collection.count_documents({"method": "PUT"}),
-        "PATCH": collection.count_documents({"method": "PATCH"}),
-        "DELETE": collection.count_documents({"method": "DELETE"})
-    }
+def log_stats():
+    """Fetch and print statistics from the Nginx logs in MongoDB.
+    
+    This function connects to the MongoDB server, retrieves the total
+    number of log entries, counts the occurrences of specific HTTP methods,
+    and counts the number of GET requests to the '/status' path.
+    """
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        logs_collection = client.logs.nginx
+        total = logs_collection.count_documents({})
+        get = logs_collection.count_documents({"method": "GET"})
+        post = logs_collection.count_documents({"method": "POST"})
+        put = logs_collection.count_documents({"method": "PUT"})
+        patch = logs_collection.count_documents({"method": "PATCH"})
+        delete = logs_collection.count_documents({"method": "DELETE"})
+        path = logs_collection.count_documents({"method": "GET", "path": "/status"})
+        
+        print(f"{total} logs")
+        print("Methods:")
+        print(f"\tmethod GET: {get}")
+        print(f"\tmethod POST: {post}")
+        print(f"\tmethod PUT: {put}")
+        print(f"\tmethod PATCH: {patch}")
+        print(f"\tmethod DELETE: {delete}")
+        print(f"{path} status check")
+    
+    except errors.ConnectionError:
+        print("Could not connect to MongoDB.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    # Get count for GET requests to /status
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-
-    # Display results
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in methods_count.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check} status check")
 
 if __name__ == "__main__":
-    main()
+    log_stats()
